1939e2d4530f030488e6bd5c6a79ef1d80fbee32
jake.baker0630 <jake.baker0630@gmail.com>
2023-03-10 21:15:22 -0800

    debugger

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
 #include <stdio.h>
-#include <SDL.h>
+#include "SDL.h"
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
-int main(int argc, char** argv) 
+#define _USE_SDL_
+#ifdef _USE_SDL_
+void sdl() 
 {
    if (SDL_Init(SDL_INIT_VIDEO) < 0){
         printf("Could not init SDL ::SDL_ERROR:: {}\n");
     }
 
-    SDL_Window* window = SDL_CreateWindow("Fun With Data Structures", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    SDL_Window* window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
     if (!window){
         printf("SDL Window could not be created :: SDL_ERROR :: {}\n");
     }
@@ -23,6 +25,11 @@ int main(int argc, char** argv)
     }
 
     printf("Successfully created SDL Window\n");
+}
+#endif
 
+int main(int argc, char** argv) 
+{
+    sdl();
     return 0;
 }
