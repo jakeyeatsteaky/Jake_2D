9a4186769e5959f1f2da44d4db01cbcbdf595311
jake.baker0630 <jake.baker0630@gmail.com>
2023-03-11 11:10:31 -0800

    Rote Observer pattern setup

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
+#include <forward_list>
+#include <iostream>
+#include <memory>
 #include <stdio.h>
+#include <string>
+
 #include "SDL.h"
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
-#define _USE_SDL_
 #ifdef _USE_SDL_
 void sdl() 
 {
@@ -28,8 +32,62 @@ void sdl()
 }
 #endif
 
+class Observer
+{
+public:
+    Observer() = default;
+    Observer(std::string n) : name(n) {}
+    ~Observer () {}
+
+    void onNotifyAll() {
+        std::cout << name << " says hello \n";
+    }
+
+    std::string getName() {return name;}
+private:
+    std::string name;
+};
+
+class Subject
+{
+public:
+    Subject() = default;
+    ~Subject() {}
+    
+    void AddObserver(Observer* ob) {
+        observers.push_front(ob);
+    }
+
+    void RemoveObserver(Observer* ob) {
+        observers.remove(ob);
+    }
+
+    void NotifyAll() {
+        for(Observer* o : observers) {
+            o->onNotifyAll();
+        }
+    }
+
+private:
+    std::string name;
+    std::forward_list<Observer*> observers; // Consideration: Does this subject have ownership of all these observers?  Something to think about when using this raw pointer or smart pointers
+    
+};
+
+
 int main(int argc, char** argv) 
 {
-    sdl();
+    Subject subject;
+    Observer ob  = Observer("ob");
+    subject.AddObserver(&ob);
+    subject.AddObserver(new Observer("Ob 2"));
+    subject.AddObserver(new Observer("Ob 3"));
+
+    subject.NotifyAll();
+
+    subject.RemoveObserver(&ob);
+
+    subject.NotifyAll();
+
     return 0;
 }
